9a0c63c29007da2e0ee55c7bd9b97f4c961d6af7
theadg <deguzmanbernardandrew@gmail.com>
2023-01-19 13:17:51 +0800

    Set icon images

---
+++ src/components/formIcons.js
@@ -0,0 +1,9 @@
+import arcadeIcon from '../assets/icon-arcade.svg';
+import advancedIcon from '../assets/icon-advanced.svg';
+import proIcon from '../assets/icon-pro.svg';
+import thanksIcon from '../assets/icon-thank-you.svg';
+
+document.querySelector('#arcadeImg').src = arcadeIcon;
+document.querySelector('#advancedImg').src = advancedIcon;
+document.querySelector('#proImg').src = proIcon;
+document.querySelector('#thanksImg').src = thanksIcon;
